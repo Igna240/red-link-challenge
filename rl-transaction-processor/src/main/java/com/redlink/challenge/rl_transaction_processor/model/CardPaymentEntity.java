8869bec57e7690acc40484940cda12772862f9fe
Igna240 <brezanignacio@gmail.com>
2025-01-24 23:41:08 -0300

    remove Entity anotation

--- rl-transaction-processor/src/main/java/com/redlink/challenge/rl_transaction_processor/model/CardPaymentEntity.java
+++ rl-transaction-processor/src/main/java/com/redlink/challenge/rl_transaction_processor/model/CardPaymentEntity.java
@@ -1,9 +1,7 @@
 package com.redlink.challenge.rl_transaction_processor.model;
 
-import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 
-@Entity
 @Table(name = "rl_card_payment")
 public class CardPaymentEntity {
 }
